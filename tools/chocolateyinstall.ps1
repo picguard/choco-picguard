97338abacece12f1519b1ddc3548a94c35eaddec
kjxbyz <kjxbyz888@outlook.com>
2024-07-30 20:14:35 +0800

    chore: v1.0.0-rc332

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,21 +1,24 @@
 
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/picguard/picguard/releases/download/v1.0.0%2B298/picguard_1.0.0+298_windows_x86_64.exe'
-$url64      = 'https://github.com/picguard/picguard/releases/download/v1.0.0%2B298/picguard_1.0.0+298_windows_x86_64.exe'
+$url64      = 'https://github.com/picguard/picguard/releases/download/v1.0.0%2B332/picguard_1.0.0+332_windows_x64.exe'
+
+$arch = [System.Environment]::Is64BitOperatingSystem
+
+if (-not $arch) {
+  Write-Error "This package does not support x86 architecture. Installation is not allowed."
+  exit 1
+}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url
   url64bit      = $url64
 
   softwareName  = 'picguard*'
 
-  checksum      = '470A73C3EFF80C820F33D5FDE83D31449D83E677398CE130C7094BDE95E11491'
-  checksumType  = 'sha256'
-  checksum64    = '470A73C3EFF80C820F33D5FDE83D31449D83E677398CE130C7094BDE95E11491'
+  checksum64    = '6B5A7596C10F4F1004CA64A59DDDEC3033CA3D4E3B8A9F87A49D1F1DC03A30D1'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
