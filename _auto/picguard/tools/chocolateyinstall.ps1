04663b41c380af417f6aac7247b4681adf31220c
kjxbyz <kjxbyz888@outlook.com>
2024-09-22 10:08:21 +0000

    Apply Choco package update - picguard 1.0.2.409
    Signed-off-by: kjxbyz <kjxbyz888@outlook.com>

--- _auto/picguard/tools/chocolateyinstall.ps1
+++ _auto/picguard/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/picguard/picguard/releases/download/v1.0.0%2B399/picguard_1.0.0%2B399_windows_x64.exe'
+$url64      = 'https://github.com/picguard/picguard/releases/download/v1.0.2%2B409/picguard_1.0.2%2B409_windows_x64.exe'
 
 $arch = [System.Environment]::Is64BitOperatingSystem
 
@@ -18,7 +18,7 @@ $packageArgs = @{
 
   softwareName  = 'picguard*'
 
-  checksum64    = 'b007e0170dfbff813070cfcda3c4e3f27901766656c17b7470d1f63ccde3a430'
+  checksum64    = 'ae9c6083e6eb0f8a1e4dfee674ca939bf4f8a5a1725fc72cb7af075dd88319c3'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
